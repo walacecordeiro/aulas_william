8d5d168fec82a8418440d674208ee5bf188f2169
walacecordeiro <walacecordeirodossantos@gmail.com>
2025-01-24 16:31:06 -0300

    Aula 04 adicionada

--- testes/main.py
+++ testes/main.py
@@ -2,4 +2,5 @@ for n in range(2, 10):
     for x in range(2, n): # no primeiro loop, n = 2, x = 2, então não existe indice no range(2, 2), ou seja, não entra no loop!!! Sempre que n for igual a x, não entra no loop.
         if n % x == 0:
             print(f"{n} igual a {x} * {n//x}") # se o resto da divisão de n por x for igual a 0, então imprime a mensagem.
-            break
+            continue
+        print(f"{n} não é um número primo")
